f687f46f27dd20327a2a5ac70a168fb8d89351a4
Khingdave <dheyved1@gmail.com>
2022-03-02 16:55:53 +0100

    updated!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
+import { ContactComponent } from './modules/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/security/security.component.ts
+++ src/app/modules/security/security.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityComponent implements OnInit {
 
+  account: boolean = true;
+  controls: boolean = false;
+  assets: boolean = false;
+  compliance: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAccount() {
+    this.account = true
+    this.controls = false
+    this.assets = false
+    this.compliance = false
+  }
+  showControls() {
+    this.account = false
+    this.controls = true
+    this.assets = false
+    this.compliance = false
+  }
+  showAssets() {
+    this.account = false
+    this.controls = false
+    this.assets = true
+    this.compliance = false
+  }
+  showCompliance() {
+    this.account = false
+    this.controls = false
+    this.assets = false
+    this.compliance = true
   }
 
 }
